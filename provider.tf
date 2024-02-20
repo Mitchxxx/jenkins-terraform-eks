e11fb253ee625688c08765341c7b6c8183ce034d
Mitchxxx <megboko@gmail.com>
2024-02-20 12:59:24 +0000

    Update

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "ibtlearning-terraform"
+    key            = "ibtlearning-terraform/Eks"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "eu-west-1"
 }
